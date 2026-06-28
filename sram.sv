388b1e5818581242c2508aaf5a946a9b42cb23f8
Brian Duong <133903181+ohmy90@users.noreply.github.com>
2026-06-28 01:03:01 -0400

    Update sram.sv

--- sram.sv
+++ sram.sv
@@ -1,6 +1,6 @@
 module sram #(parameter int DEPTH = 16, parameter int WIDTH = 8) (
-  input  logic                      clk,
-  input  logic                      we,
+  input  logic clk,
+  input  logic we,
   input  logic [$clog2(DEPTH)-1:0] addr,
   input  logic [WIDTH-1:0]         wdata,
   output logic [WIDTH-1:0]         rdata

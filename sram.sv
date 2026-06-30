dd9817c8bf78168e2b097fe1190554d06fbfee9d
Brian Duong <133903181+ohmy90@users.noreply.github.com>
2026-06-29 21:10:56 -0400

    Update sram.sv

--- sram.sv
+++ sram.sv
@@ -1,13 +1,24 @@
+interface sram_if #(parameter int DEPTH = 16, parameter int WIDTH = 8) (input logic clk);
+  logic wr_en;
+  logic rd_en;
+  logic rst_n;
+  logic [$clog2(DEPTH)-1:0] addr;
+  logic [WIDTH-1:0] wdata;
+  logic [WIDTH-1:0] rdata;
+
+endinterface
+
+
 module sram #(parameter int DEPTH = 16, parameter int WIDTH = 8) (
-  input  logic clk,
-  input  logic we,
-  input  logic [$clog2(DEPTH)-1:0] addr,
-  input  logic [WIDTH-1:0]         wdata,
-  output logic [WIDTH-1:0]         rdata
+  sram_if vif
 );
   logic [WIDTH-1:0] mem [0:DEPTH-1];
-  always_ff @(posedge clk) begin
-    if (we) mem[addr] <= wdata;
-    rdata <= mem[addr];
+
+  always_ff @(posedge vif.clk or negedge vif.rst_n) begin
+    if (!vif.rst_n) vif.rdata <= '0;
+    else begin
+      if (vif.wr_en) mem[vif.addr] <= vif.wdata;
+      if (vif.rd_en) vif.rdata <= mem[vif.addr];
+    end
   end
 endmodule
